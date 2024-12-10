c3325bc5fecde920668d607fecf44f2ea9fa2a0f
@Omner3 <gonzalezomner@hotmail.com>
2024-12-10 13:26:28 -0600

    selectors

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
+const mortgageAmount = document.querySelector('#mortgageAmount')
+const mortgageTerm = document.querySelector('#mortgageTerm')
+const interesRate = document.querySelector('#interesRate')
+const repaymentRadio = document.querySelector('#repaymentRadio')
+const interestRadio = document.querySelector('#interestRadio')
+const caculateRepayments = document.querySelector('#caculate-repayments')
+
+
+
+
+//validación
 (() => {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+
     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
+
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
